a0647baf683bba21eaff1c9b6f6cb8688f7b2d4d
v_liheng02 <v_liheng02@MacBook-Pro.local>
2026-05-09 16:16:08 +0800

    修复dispatch_permissions表列名不匹配：can_dispatch_to→dispatch_scope

--- model/dispatch.go
+++ model/dispatch.go
@@ -5,7 +5,7 @@ type DispatchPermission struct {
 	ID            uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	UnitID        *uint  `json:"unit_id" gorm:"column:unit_id"`
 	UnitName      string `json:"unit_name" gorm:"column:unit_name;size:128"`
-	CanDispatchTo string `json:"can_dispatch_to" gorm:"column:can_dispatch_to;type:text"`
+	CanDispatchTo string `json:"can_dispatch_to" gorm:"column:dispatch_scope;type:json"`
 }
 
 func (DispatchPermission) TableName() string { return "dispatch_permissions" }
